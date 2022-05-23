1f75a1257541550df5d4e46d52aa190f068d5c23
Antonio Caetano Neves Neto <antonioneto@morango.DCC>
2022-05-23 14:47:53 -0300

    Finish LAB 6

---
+++ SEMESTRES/1/PDS/lab6/funcao3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int primo(long long int n) {
+    long long int i;
+    for(i = 2; i*i <= n; i++)
+        if(n % i == 0)
+            return 0;
+    return 1;
+}
+
+int main() {
+    int n;
+
+    while(scanf("%d", &n) != EOF) {
+        long long int c = 0, p = 2, s = 0;
+
+        while(c < n) {
+            if(primo(p)) {
+                s += p;
+                c++;
+            }
+            p++;
+        }
+
+        printf("%d\n", s);
+    }
+
+    system("pause");
+    return 0;
+}
---
+++ SEMESTRES/1/PDS/lab6/funcao4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int bissexto(int y) {
+    return y % 4 == 0 && (y % 100 != 0 || y % 400 == 0);
+}
+
+int main() {
+    int y;
+
+    while(scanf("%d", &y) != EOF) {
+        printf("%d\n", bissexto(y));
+    }
+
+    system("pause");
+    return 0;
+}
